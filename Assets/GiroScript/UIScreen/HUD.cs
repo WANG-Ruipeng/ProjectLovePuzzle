fe1fa9ae501383bcfb9cda83bbe5ba45d60c3fe8
unknown <3139106326@qq.com>
2023-03-14 20:28:50 +0800

    HUD和UIManager的搭建

---
+++ Assets/GiroScript/UIScreen/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HyperCasual.Core;
+using TMPro;
+using UnityEngine.UI;
+namespace Giro
+{
+    public class HUD : View
+    {
+        [Header("UI On Scene")]
+        public TextMeshProUGUI countdown;
+        public Image leftIndicator;
+        public Image rightIndicator;
+
+
+
+        [Header("interaction Object（暂时没用）")]
+        public HyperCasualButton pauseButton;
+        public AbstractGameEvent pauseEvent;
+
+        int timeLeft;
+        public float TimeLeft
+        {
+            get => timeLeft;
+            set
+            {
+                timeLeft = (int)Mathf.Ceil(value);
+                countdown.text = timeLeft.ToString();
+            }
+        }
+
+
+    }
+}
